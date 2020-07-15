cc7f297118578c94684ea899616ae76691428f24
Ishank Sharma <ishankdev@gmail.com>
2020-07-16 01:12:39 +0530

    refactor and add show directions to reach the alert

--- dashboard/assets/js/firebase.js
+++ dashboard/assets/js/firebase.js
@@ -2,47 +2,59 @@ const alertList = document.querySelector('.alerts');
 
 // function to set attributes for different elements
 function setAttributes(el, attrs) {
-  for(var key in attrs) {
-    el.setAttribute(key, attrs[key]);
-  }
+    for (var key in attrs) {
+        el.setAttribute(key, attrs[key]);
+    }
+}
+// Function that returns name of the place from latitude and longitude works only if the lat,long are exact and have more decimal places
+function reverseGeocoding(latitude, longitude) {
+    fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/" + latitude + "," + longitude + ".json?types=poi&access_token=pk.eyJ1IjoiaXNoYW5rNDciLCJhIjoiY2tibm8xbHF3MTc1ZDJycW55Znh6YWppdSJ9.zCHaLlcI36pxVctNkFZ5Sg").then(
+        res => {
+            res.json().then(
+                data => {
+                    return data.features[0].place_name;
+                }
+            )
+        }
+    );
 }
 
-function renderAlert(doc){
+function renderAlert(doc) {
     count_id = 0;
     let li = document.createElement('li');
-    setAttributes(li, {'class' : 'list-group-item dropdown', 'id' : doc.id});
+    setAttributes(li, { 'class': 'list-group-item dropdown', 'id': doc.id });
 
     let message = document.createElement('button');
-    message.innerHTML = 'Alert: '+ doc.data().alert;
-    setAttributes(message, {'class' : 'alertMessage mb-2 mr-2 dropdown-toggle btn btn-danger', 'data-toggle' : 'dropdown'});
+    message.innerHTML = 'Alert: ' + doc.data().alert;
+    setAttributes(message, { 'class': 'alertMessage mb-2 mr-2 dropdown-toggle btn btn-danger', 'data-toggle': 'dropdown' });
     let menu = document.createElement('div');
-    setAttributes(menu, {'class' : 'dropdown-menu', 'tabIndex' : -1})
+    setAttributes(menu, { 'class': 'dropdown-menu', 'tabIndex': -1 })
 
     let divider = document.createElement('div');
-    setAttributes(divider, {'class' : 'dropdown-divider', 'tabIndex' : -1});
+    setAttributes(divider, { 'class': 'dropdown-divider', 'tabIndex': -1 });
 
     let coordinates = document.createElement('li');
-//Round off the coordinates to 3 decimal places
+    //Round off the coordinates to 3 decimal places
     // doc.data().coordinates[0] = Number(parseFloat(doc.data().coordinates[0]).toFixed(3));
     // doc.data().coordinates[1] = Number(parseFloat(doc.data().coordinates[1]).toFixed(3));
-    coordinates.innerHTML ='<strong>Coordinates</strong>: '+ doc.data().location.latitude + ' ' + doc.data().location.longitude;
-    setAttributes(coordinates, {'class' : 'dropdown-item', 'tabIndex' : 0});
+    coordinates.innerHTML = '<strong>Coordinates</strong>: ' + doc.data().location.latitude + ' ' + doc.data().location.longitude;
+    setAttributes(coordinates, { 'class': 'dropdown-item', 'tabIndex': 0 });
 
     let time = document.createElement('li');
-    time.innerHTML = '<strong>Time: </strong>: '+doc.data().timeStamp;
-    setAttributes(time, {'class' : 'dropdown-item', 'tabIndex' : 0});
+    time.innerHTML = '<strong>Time: </strong>: ' + doc.data().timeStamp;
+    setAttributes(time, { 'class': 'dropdown-item', 'tabIndex': 0 });
 
     let uniqueId = document.createElement('li');
-    uniqueId.innerHTML = '<strong>ID: </strong>'+doc.id;
-    setAttributes(uniqueId, {'class' : 'dropdown-item', 'tabIndex' : 0});
+    uniqueId.innerHTML = '<strong>ID: </strong>' + doc.id;
+    setAttributes(uniqueId, { 'class': 'dropdown-item', 'tabIndex': 0 });
 
     let nameOfSender = document.createElement('li');
-    nameOfSender.innerHTML ='<strong>Name : </strong>'+ doc.data().sender;
-    setAttributes(nameOfSender, {'class' : 'dropdown-item', 'tabIndex' : 0});
+    nameOfSender.innerHTML = '<strong>Name : </strong>' + doc.data().sender;
+    setAttributes(nameOfSender, { 'class': 'dropdown-item', 'tabIndex': 0 });
 
     let locateOnMap = document.createElement('li');
     locateOnMap.textContent = 'Locate';
-    setAttributes(locateOnMap, {'class' : 'dropdown-item font-weight-bold text-danger', 'tabIndex' : 0});
+    setAttributes(locateOnMap, { 'class': 'dropdown-item font-weight-bold text-danger', 'tabIndex': 0 });
 
     menu.appendChild(uniqueId);
     menu.appendChild(divider);
@@ -55,30 +67,35 @@ function renderAlert(doc){
     li.appendChild(message);
     li.appendChild(menu);
     alertList.appendChild(li);
+    console.log(doc.data().alert)
+    // event listener added to alert button
 
-// event listener added to alert button
-    mapAlert = 'id:'+doc.id+'<br>name :'+doc.data().sender+'<br>message :'+doc.data().alert+'<br>Time:'+doc.data().timeStamp;
     var location = []
-    location.push(doc.data().location.longitude); 
-    location.push(doc.data().location.latitude);
-    locateOnMap.addEventListener('click', function() {
-        var marker = new mapboxgl.Marker()
-        .setLngLat(location)
-        .addTo(map);
-        
-        var popup = new mapboxgl.Popup({ closeOnClick: false })
-        .setLngLat(location)
-        .setHTML(mapAlert)
-        .addTo(map);
-        map.flyTo({
-        center: location,
-        essential: true // this animation is considered essential with respect to prefers-reduced-motion
-        });
-        });
-}
-var docRef = db.collection('Emergency Mode').get().then((snapshot) =>{
+    latitude = doc.data().location.latitude;
+    longitude = doc.data().location.longitude;
+    location.push(longitude);
+    location.push(latitude);
 
-    snapshot.docs.forEach(doc=>{
+    locateOnMap.addEventListener('click', function () {
+
+        mapAlert = '<strong>id: </strong>' + doc.id + '<br><strong>Name: </strong> :' + doc.data().sender + '<br><strong>Message: </strong>' + '<br><strong>Location: </strong>' + location + '<br><strong>Time: </strong>' + new Date(doc.data().timeStamp) + '<br><strong>Place Name: </strong>' + reverseGeocoding(latitude, longitude);
+        var marker = new mapboxgl.Marker()
+            .setLngLat(location)
+            .addTo(map);
+
+        var popup = new mapboxgl.Popup({ closeOnClick: false })
+            .setLngLat(location)
+            .setHTML(mapAlert)
+            .addTo(map);
+        map.flyTo({
+            center: location,
+            essential: true // this animation is considered essential with respect to prefers-reduced-motion
+        });
+    });
+}
+var docRef = db.collection('Emergency Mode').get().then((snapshot) => {
+
+    snapshot.docs.forEach(doc => {
         renderAlert(doc);
     })
 });
--- dashboard/assets/js/index.js
+++ dashboard/assets/js/index.js
@@ -1,45 +1,45 @@
-function wait(ms){
+function wait(ms) {
     var start = new Date().getTime();
     var end = start;
-    while(end < start + ms) {
-      end = new Date().getTime();
-   }
- }
+    while (end < start + ms) {
+        end = new Date().getTime();
+    }
+}
 // Police fetch
 fetch("http://aman28.pythonanywhere.com/police/").then(
-    res=>{
+    res => {
         res.json().then(
-            data=>{
-                    var temp = "";
-                    data.forEach((u)=>{
-                        temp+="<tr>";
-                        temp += "<td>"+u.id+"</td>";
-                        temp += "<td>"+ u.name+"</td>";
-                        temp += "<td>"+u.rank + "</td>";
-                    });
-                    totalPolice = data.length
-                    console.log(totalPolice)
-                    document.getElementById("police").innerHTML = temp;
+            data => {
+                var temp = "";
+                data.forEach((u) => {
+                    temp += "<tr>";
+                    temp += "<td>" + u.id + "</td>";
+                    temp += "<td>" + u.name + "</td>";
+                    temp += "<td>" + u.rank + "</td>";
+                });
+                totalPolice = data.length
+                console.log(totalPolice)
+                document.getElementById("police").innerHTML = temp;
             }
         )
     }
 );
 // Zone fetch
 fetch("http://aman28.pythonanywhere.com/zone/").then(
-    res=>{
+    res => {
         res.json().then(
-            data=>{
-                    var temp = "";
-                    data.forEach((u)=>{
-                        temp+="<tr>";
-                        temp += "<td>"+u.id+"</td>";
-                        temp += "<td>"+ u.name+"</td>";
-                        temp += "<td>"+u.priority + "</td>";
-                        temp += "<td class = btn btn-primary><a href = 'http://aman28.pythonanywhere.com/admin/police/polices/"+u.id+"/change/'>EDIT</td>";
-                    });
-                    document.getElementById("zones").innerHTML = temp;
-                    totalZones = data.length
-                    console.log(data.length)
+            data => {
+                var temp = "";
+                data.forEach((u) => {
+                    temp += "<tr>";
+                    temp += "<td>" + u.id + "</td>";
+                    temp += "<td>" + u.name + "</td>";
+                    temp += "<td>" + u.priority + "</td>";
+                    temp += "<td class = btn btn-primary><a href = 'http://aman28.pythonanywhere.com/admin/police/polices/" + u.id + "/change/'>EDIT</td>";
+                });
+                document.getElementById("zones").innerHTML = temp;
+                totalZones = data.length
+                console.log(data.length)
             }
         )
     }
@@ -48,35 +48,35 @@ fetch("http://aman28.pythonanywhere.com/zone/").then(
 wait(100) // prevent fetching this before police and zone fetch
 
 fetch("http://aman28.pythonanywhere.com/allocation/").then(
-    res=>{
+    res => {
         res.json().then(
-            data=>{
-                    var temp = "";
-                    data.forEach((u)=>{
-                        temp+="<tr>";
-                        temp += "<td>"+u.id+"</td>";
-                        temp += "<td>"+u.zone+"</td>";
-                        temp += "<td>"+u.priority + "</td>";
-                        temp += "<td>"+u.police_allotted + "</td>";
-                        u.date_posted = u.date_posted.replace('Z', ' ').replace('T', ' ');
-                        temp += "<td>"+u.date_posted + "</td>";
-                        temp += "<td>"+u.time_slot + "</td>";
-                        temp += "<td class = btn btn-primary><a href = 'http://aman28.pythonanywhere.com/admin/allocations/allocations/"+u.id+"/change/'>EDIT</td>";
-                    });
-                    document.getElementById("allocation").innerHTML = temp;
-                    //calculate zones alloted 
-                    zoneAlloted = data.length
-                    document.getElementById('zone-alloted-live').innerHTML = "<h4>"+ zoneAlloted + "/"+totalZones+" Alloted</h4>"
-                    //calculate zones not alloted
-                    zoneNotAlloted = totalZones-data.length
-                    document.getElementById('zone-na-live').innerHTML = "<h4>"+ zoneNotAlloted + "/"+totalZones+" Not Deployed</h4>"
-                    // calculate police alloted
-                    policeAlloted = data.length
-                    document.getElementById("onduty-live").innerHTML = "<h4>"+policeAlloted+ "/"+totalPolice+" Deployed</h4>";
+            data => {
+                var temp = "";
+                data.forEach((u) => {
+                    temp += "<tr>";
+                    temp += "<td>" + u.id + "</td>";
+                    temp += "<td>" + u.zone + "</td>";
+                    temp += "<td>" + u.priority + "</td>";
+                    temp += "<td>" + u.police_allotted + "</td>";
+                    u.date_posted = u.date_posted.replace('Z', ' ').replace('T', ' ');
+                    temp += "<td>" + u.date_posted + "</td>";
+                    temp += "<td>" + u.time_slot + "</td>";
+                    temp += "<td class = btn btn-primary><a href = 'http://aman28.pythonanywhere.com/admin/allocations/allocations/" + u.id + "/change/'>EDIT</td>";
+                });
+                document.getElementById("allocation").innerHTML = temp;
+                //calculate zones alloted 
+                zoneAlloted = data.length
+                document.getElementById('zone-alloted-live').innerHTML = "<h4>" + zoneAlloted + "/" + totalZones + " Alloted</h4>"
+                //calculate zones not alloted
+                zoneNotAlloted = totalZones - data.length
+                document.getElementById('zone-na-live').innerHTML = "<h4>" + zoneNotAlloted + "/" + totalZones + " Not Deployed</h4>"
+                // calculate police alloted
+                policeAlloted = data.length
+                document.getElementById("onduty-live").innerHTML = "<h4>" + policeAlloted + "/" + totalPolice + " Deployed</h4>";
 
 
             }
         )
     }
 );
-// wait(1000) 
+// wait(1000)
