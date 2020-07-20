5bbf2544e72043ce41f48ea4ceb3eaa602dea3e3
Ishank Sharma <ishankdev@gmail.com>
2020-07-20 21:23:27 +0530

    admin dashboard + reverse Geocoding API working

--- dashboard/assets/js/firebase.js
+++ dashboard/assets/js/firebase.js
@@ -6,20 +6,17 @@ function setAttributes(el, attrs) {
         el.setAttribute(key, attrs[key]);
     }
 }
+
 // Function that returns name of the place from latitude and longitude works only if the lat,long are exact and have more decimal places
-function reverseGeocoding(latitude, longitude) {
-    fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/" + latitude + "," + longitude + ".json?types=poi&access_token=pk.eyJ1IjoiaXNoYW5rNDciLCJhIjoiY2tibm8xbHF3MTc1ZDJycW55Znh6YWppdSJ9.zCHaLlcI36pxVctNkFZ5Sg").then(
-        res => {
-            res.json().then(
-                data => {
-                    return data.features[0].place_name;
-                }
-            )
-        }
-    );
+
+async function reverseGeocoding(latitude, longitude) {
+    let response = await fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/" + longitude + ',' + latitude + ".json?access_token=pk.eyJ1IjoiaXNoYW5rNDciLCJhIjoiY2tibm8xbHF3MTc1ZDJycW55Znh6YWppdSJ9.zCHaLlcI36pxVctNkFZ5Sg");
+    let data = await response.json()
+    return data;
 }
 
-function renderAlert(doc) {
+// Function that renders alert on sidebar from firebase
+async function renderAlert(doc) {
     count_id = 0;
     let li = document.createElement('li');
     setAttributes(li, { 'class': 'list-group-item dropdown', 'id': doc.id });
@@ -34,9 +31,7 @@ function renderAlert(doc) {
     setAttributes(divider, { 'class': 'dropdown-divider', 'tabIndex': -1 });
 
     let coordinates = document.createElement('li');
-    //Round off the coordinates to 3 decimal places
-    // doc.data().coordinates[0] = Number(parseFloat(doc.data().coordinates[0]).toFixed(3));
-    // doc.data().coordinates[1] = Number(parseFloat(doc.data().coordinates[1]).toFixed(3));
+
     coordinates.innerHTML = '<strong>Coordinates</strong>: ' + doc.data().location.latitude + ' ' + doc.data().location.longitude;
     setAttributes(coordinates, { 'class': 'dropdown-item', 'tabIndex': 0 });
 
@@ -67,18 +62,19 @@ function renderAlert(doc) {
     li.appendChild(message);
     li.appendChild(menu);
     alertList.appendChild(li);
-    console.log(doc.data().alert)
-    // event listener added to alert button
 
-    var location = []
-    latitude = doc.data().location.latitude;
-    longitude = doc.data().location.longitude;
-    location.push(longitude);
-    location.push(latitude);
 
-    locateOnMap.addEventListener('click', function () {
 
-        mapAlert = '<strong>id: </strong>' + doc.id + '<br><strong>Name: </strong> :' + doc.data().sender + '<br><strong>Message: </strong>' + '<br><strong>Location: </strong>' + location + '<br><strong>Time: </strong>' + new Date(doc.data().timeStamp) + '<br><strong>Place Name: </strong>' + reverseGeocoding(latitude, longitude);
+    locateOnMap.addEventListener('click', async function () {
+
+        var location = []
+        latitude = doc.data().location.latitude;
+        longitude = doc.data().location.longitude;
+        location.push(longitude);
+        location.push(latitude);
+        reverseLatLong = await reverseGeocoding(latitude, longitude);
+
+        mapAlert = '<strong>id: </strong>' + doc.id + '<br><strong>Name: </strong> :' + doc.data().sender + '<br><strong>Message: </strong>' + doc.data().alert + '<br><strong>Location: </strong>' + location + '<br><strong>Time: </strong>' + new Date(doc.data().timeStamp) + '<br><strong>Place Name: </strong>' + reverseLatLong.features[0].place_name.split(',');
         var marker = new mapboxgl.Marker()
             .setLngLat(location)
             .addTo(map);
@@ -93,6 +89,7 @@ function renderAlert(doc) {
         });
     });
 }
+// Function that iterates over firebase
 var docRef = db.collection('Emergency Mode').get().then((snapshot) => {
 
     snapshot.docs.forEach(doc => {
--- dashboard/assets/js/index.js
+++ dashboard/assets/js/index.js
@@ -5,48 +5,7 @@ function wait(ms) {
         end = new Date().getTime();
     }
 }
-// Police fetch
-fetch("http://aman28.pythonanywhere.com/police/").then(
-    res => {
-        res.json().then(
-            data => {
-                var temp = "";
-                data.forEach((u) => {
-                    temp += "<tr>";
-                    temp += "<td>" + u.id + "</td>";
-                    temp += "<td>" + u.name + "</td>";
-                    temp += "<td>" + u.rank + "</td>";
-                });
-                totalPolice = data.length
-                console.log(totalPolice)
-                document.getElementById("police").innerHTML = temp;
-            }
-        )
-    }
-);
-// Zone fetch
-fetch("http://aman28.pythonanywhere.com/zone/").then(
-    res => {
-        res.json().then(
-            data => {
-                var temp = "";
-                data.forEach((u) => {
-                    temp += "<tr>";
-                    temp += "<td>" + u.id + "</td>";
-                    temp += "<td>" + u.name + "</td>";
-                    temp += "<td>" + u.priority + "</td>";
-                    temp += "<td class = btn btn-primary><a href = 'http://aman28.pythonanywhere.com/admin/police/polices/" + u.id + "/change/'>EDIT</td>";
-                });
-                document.getElementById("zones").innerHTML = temp;
-                totalZones = data.length
-                console.log(data.length)
-            }
-        )
-    }
-);
-// Police fetch
-wait(100) // prevent fetching this before police and zone fetch
-
+// Fetch allocation table
 fetch("http://aman28.pythonanywhere.com/allocation/").then(
     res => {
         res.json().then(
@@ -55,28 +14,35 @@ fetch("http://aman28.pythonanywhere.com/allocation/").then(
                 data.forEach((u) => {
                     temp += "<tr>";
                     temp += "<td>" + u.id + "</td>";
-                    temp += "<td>" + u.zone + "</td>";
-                    temp += "<td>" + u.priority + "</td>";
-                    temp += "<td>" + u.police_allotted + "</td>";
-                    u.date_posted = u.date_posted.replace('Z', ' ').replace('T', ' ');
-                    temp += "<td>" + u.date_posted + "</td>";
-                    temp += "<td>" + u.time_slot + "</td>";
-                    temp += "<td class = btn btn-primary><a href = 'http://aman28.pythonanywhere.com/admin/allocations/allocations/" + u.id + "/change/'>EDIT</td>";
+                    temp += "<td>" + u.zone_name + "</td>";
+                    temp += "<td>" + u.police_name + "</td>";
+                    temp += "<td>" + new Date(u.date_posted).toLocaleString() + "</td>";
+                    temp += "<td>" + u.time + "</td>";
                 });
-                document.getElementById("allocation").innerHTML = temp;
-                //calculate zones alloted 
-                zoneAlloted = data.length
-                document.getElementById('zone-alloted-live').innerHTML = "<h4>" + zoneAlloted + "/" + totalZones + " Alloted</h4>"
-                //calculate zones not alloted
-                zoneNotAlloted = totalZones - data.length
-                document.getElementById('zone-na-live').innerHTML = "<h4>" + zoneNotAlloted + "/" + totalZones + " Not Deployed</h4>"
-                // calculate police alloted
-                policeAlloted = data.length
-                document.getElementById("onduty-live").innerHTML = "<h4>" + policeAlloted + "/" + totalPolice + " Deployed</h4>";
-
-
             }
         )
     }
 );
-// wait(1000)
+// fetch zone count
+fetch("http://aman28.pythonanywhere.com/zonecount/").then(
+    res => {
+        res.json().then(
+            data => {
+                console.log(data);
+                document.getElementById('zone-alloted-live').innerHTML = "<h5>" + data.alloted_zones + "/" + data.total_zones + " Alloted</h5>";
+                document.getElementById('zone-na-live').innerHTML = "<h5>" + data.unalloted_zones + "/" + data.total_zones + " Not Deployed</h5>"
+            }
+        )
+    }
+);
+// fetch police count
+fetch("http://aman28.pythonanywhere.com/policecount/").then(
+    res => {
+        res.json().then(
+            data => {
+                console.log(data);
+                document.getElementById("onduty-live").innerHTML = "<h5>" + data.alloted_police + "/" + data.total_police + " Deployed</h5>";
+            }
+        )
+    }
+);
